f3cf383e21fc754800b02e5d833111d5d4a6238a
mun-ahmd <muneeb0ahmed3@gmail.com>
2023-07-18 15:12:10 +0530

    changed message

--- recommendations.py
+++ recommendations.py
@@ -300,7 +300,7 @@ def recommendation():
     timer.end() 
     gc.collect()
     return {
-        'ci/cd' : 'success! 5 time',
+        'ci/cd' : 'success! fnsaoinf time',
         'error': errors,
         'user': senderInfo,
         'userInterestCount': match_df.shape[0],
