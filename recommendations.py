43f9a8c54ef881aa7d8d2b84479addfb7d16e555
mun-ahmd <muneeb0ahmed3@gmail.com>
2023-07-18 15:10:19 +0530

    cicd5

--- recommendations.py
+++ recommendations.py
@@ -300,7 +300,7 @@ def recommendation():
     timer.end() 
     gc.collect()
     return {
-        'ci/cd' : 'success! 4 time',
+        'ci/cd' : 'success! 5 time',
         'error': errors,
         'user': senderInfo,
         'userInterestCount': match_df.shape[0],
