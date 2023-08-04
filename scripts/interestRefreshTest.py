a46062e16534bc67d46ee9ea984d47e9ccd1b785
root <muneeb0ahmed3@gmail.com>
2023-08-04 10:39:15 +0000

    fixed interest updations

--- scripts/interestRefreshTest.py
+++ scripts/interestRefreshTest.py
@@ -15,11 +15,15 @@ def updateInterest():
         if(lastTimestamp is None or lastTimestamp is np.nan):
             raise Exception('last timestamp is null')
 
-        result = requests.post(INTEREST_API, data = {'timestamp' : lastTimestamp}).json()
-        print(result)
+        result = requests.post(INTEREST_API, data = {'time' : lastTimestamp}).json()
+                
         result_df = pd.DataFrame(result).rename(
             columns={'sender' : 'sender_id', 'receiver' : 'receiver_id', 'time' : 'timestamp', 'status' : 'isAccepted'}
             )
+        
+        for col in result_df.columns:
+            result_df = result_df[result_df[col] != '']
+
         result_df.isAccepted = (result_df.isAccepted != 'Pending').astype(int)
 
         result_df.sender_id = result_df.sender_id.astype(int)
@@ -27,7 +31,8 @@ def updateInterest():
         result_df.timestamp = result_df.timestamp.astype(int)
         result_df.isAccepted = result_df.isAccepted.astype(int)
 
-        interest = pd.concat([interest, result_df]).reset_index().drop(columns=['index'])
+        interest = pd.concat([interest, result_df], ignore_index=True)
+
         performWithFileLock(INTEREST_URI, lambda : interest.to_feather(INTEREST_URI))
         print(f'Updated Interests added {result_df.shape[0]}')
 
