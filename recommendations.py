106f5768890852bae3f187f68cac742e32b09de3
mun-ahmd <muneeb0ahmed3@gmail.com>
2023-08-30 17:35:20 +0530

    added null cat

--- recommendations.py
+++ recommendations.py
@@ -351,7 +351,7 @@ def createRecommendationResults(member_id, userData, offset, count, withInfo, ti
     if withInfo:
         for col in predictions:
             if predictions[col].dtype.name == 'category':
-                predictions[col].cat.add_categories([0])
+                predictions[col] = predictions[col].cat.add_categories([0])
         predictions.fillna(0, inplace=True)
     return {
         # 'error': errors,
